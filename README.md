cac7fe6ad0a3ed1abd9b40913b06141afc6ec2e7
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-11-11 07:51:22 +0100

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather App
+
+![header_image](images/weather.jpg)
+
+## Description:
+This project is a web app that uses web API and user input data to dynamically update the UI in the weather application. 
+
+## Usage:
+The web page is usable across all modern browsers.
+
+## Dependencies
+index.html style.css script.js
---
+++ script.js
@@ -0,0 +1,47 @@
+let weather = {
+    apiKey: "34a25ed6886d631e5e0316f568d5ada7",
+    fetchWeather: function (city) {
+        fetch(
+           "https://api.openweathermap.org/data/2.5/weather?q=" 
+           + city 
+           + "&units=metric&appid="
+           + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { country } = data.sys;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,country,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/hr";
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+
+
+
+}
+
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+
+document
+ .querySelector(".search-bar")
+ .addEventListener("keyup", function(event){
+     if (event.key == "Enter") {
+         weather.search();
+     }
+
+ })
